f6a22a1c10dd1b743d6757199388732b27a7a50b
Nehalokesh <nehal19697@gmail.com>
2022-10-10 18:09:14 +0530

    Task8 Commit

--- url.js
+++ url.js
@@ -1,29 +1,49 @@
 const http = require('http');
+const fs= require('fs')
 
-const server = http.createServer((req,res)=>{
+const server=http.createServer((req,res) => {
     const url = req.url;
-    if(url ==='/home'){
-        res.write('<html>');
-        res.write('<head><title>My First Page</title><head>');
-        res.write('<body><h1>Welcome to home</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/about'){
-        res.write('<html>');
-        res.write('<head><title>My First Page</title><head>')
-        res.write('<body><h1>Welcome to About Us page</h1></body>');
-        res.write('</html>');
-        return res.end()
-    }
-    if(url ==='/node'){
-        res.write('<html>');
-        res.write('<head><title>My First Page</title><head>')
-        res.write('<body><h1>Welcome to my Node JS Project</h1></body>');
-        res.write('</html>');
-        return res.end();
+    const method =req.method;
+    if(url === '/'){
+        fs.readFile("message.text",{encoding:'utf-8'},(err,data) =>{
+            if(err){
+                console.log(err)
+
+            }
+            console.log(`data from file`+data)
+            res.setHeader('Content-Type','text/html');
+            res.write('<html>')
+            res.write('<head><title> Enter message </title></head>')
+            res.write(`<body>${data}</body>`)
+            res.write('<body><form action="/message" method="POST"><input type="text"name="message"><button>Send</button></form</body>')
+            res.write('</html>')
+            return res.end()
+        });   
 
     }
-    
+    else if(url === '/message' && method === 'POST'){
+        const body =[]
+        req.on('data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody =Buffer.concat(body).toString();
+            console.log('parsedbody>>>>>',parsedBody)
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.text',message, (err) =>{
+                if(err){
+                    console.log(err)
+
+                }
+                console.log(`inside.fs.writefile`)
+                res.statusCode =302;
+                res.setHeader('location','/');
+                return res.end();
+
+
+        });
+    });
+    }
 });
 server.listen(5000);
